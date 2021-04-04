ba0717a81d0680e655a9083abe605b4d0e04e45a
Pat Polts <patriciapoltts@gmail.com>
2021-04-03 23:39:38 -0300

    rebase develop to 8bc260356e3d8fd8dbaebe3245f2927bbaac7899

--- wp-content/themes/pdm-andamento/assets/scripts/main.js
+++ wp-content/themes/pdm-andamento/assets/scripts/main.js
@@ -1,7 +1,7 @@
 require.config({
     paths: {
-        jquery: '../bower_components/jquery/dist/jquery',
-        d3: '../bower_components/d3/d3.min',
+        jquery: 'http://ec2-54-196-34-239.compute-1.amazonaws.com/wp-content/themes/pdm-andamento/node_modules/jquery/dist/jquery.min',
+        d3: 'http://ec2-54-196-34-239.compute-1.amazonaws.com/wp-content/themes/pdm-andamento/node_modules/d3/dist/d3.min',
         //leaflet: '../bower_components/leaflet/src/Leaflet',
         //leaflet: "//cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.2/leaflet",
         foundation: '../bower_components/foundation/js/foundation',
